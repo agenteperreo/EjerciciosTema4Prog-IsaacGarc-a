56cd4aff6aaf0c1c00901b7b13c826dc53224571
igarcia <igarcar@iesnervion.es>
2022-12-05 08:40:36 +0100

    5/12/2022

--- EjerciciosTema4-IsaacGarcía/src/ejercicio4/Operaciones.java
+++ EjerciciosTema4-IsaacGarcía/src/ejercicio4/Operaciones.java
@@ -2,21 +2,30 @@ package ejercicio4;
 
 public class Operaciones {
 	
+	//Creación de variables globales
 	int num1, num2;
 	
 	int suma() {
+		
+		//Devolvemos la suma
 		return num1+num2;
 	}
 	
 	int resta() {
+		
+		//Devolvemos la resta
 		return num1-num2;
 	}
 	
 	int multiplicación() {
+		
+		//Devolvemos la multiplicación
 		return num1*num2;
 	}
 	
 	double division() {
+		
+		//Devolvemos la división con decimales
 		return (double)num1/num2;
 	}
 
